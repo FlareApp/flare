efadc36894accdcb4a2dcaad88cdb92892f1fb42
hathix <neelmehta@college.harvard.edu>
2015-01-17 03:48:38 -0500

    Got carousel navigation working

--- client/js/flare.js
+++ client/js/flare.js
@@ -1,3 +1,13 @@
 if (Meteor.isClient) {
     console.log("PennApps");
+
+    // handle carousel
+    Template.body.events({
+        'click #carousel-left': function(){
+            $('.carousel').carousel('prev');
+        },
+        'click #carousel-right': function(){
+            $('.carousel').carousel('next');
+        }
+    });
 }
