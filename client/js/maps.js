d786eb31dec5e7da677a283eba7576ee5b6e5bd7
Max Wofford <max.speed.wofford@gmail.com>
2015-01-17 10:13:47 -0800

    Added map update feature

--- client/js/maps.js
+++ client/js/maps.js
@@ -2,13 +2,19 @@ if (Meteor.isClient) {
   Meteor.startup(function() {
     GoogleMaps.load();
     Geolocation.load();
+    Tracker.autorun(function () {
+      if (Geolocation.error() !== null) {
+        var center = new google.maps.LatLng(Geolocation.latLng().lat, Geolocation.latLng().lng);
+        GoogleMaps.maps.gmap.instance.panTo(center);
+      };
+    })
   });
   Template.page_map.helpers({
-    exampleMapOptions: function() {
+    gmapOptions: function() {
       // Make sure the maps API has loaded
       if (GoogleMaps.loaded()) {
-        // We can use the `ready` callback to interact with the map API once the map is ready.
-        GoogleMaps.ready('exampleMap', function(map) {
+        // We can use the `ready` callback to interact with the map API on3ce the map is ready.
+        GoogleMaps.ready('gmap', function(map) {
           // Add a marker to the map once it's ready
           var marker = new google.maps.Marker({
             position: map.options.center,
@@ -18,7 +24,7 @@ if (Meteor.isClient) {
 
         // Map initialization options
         return {
-          center: new google.maps.LatLng(Geolocation.latLng()[0] || 0, Geolocation.latLng()[1] || 0),
+          center: new google.maps.LatLng(-37.8136, 144.9631),
           zoom: 8
         };
       }
