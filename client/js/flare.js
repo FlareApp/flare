5bdc724aead39ccbff6d1d057d10ebbaa0768cb5
hathix <neelmehta@college.harvard.edu>
2015-01-17 04:00:50 -0500

    Added swiping to go between pages

--- client/js/flare.js
+++ client/js/flare.js
@@ -2,12 +2,22 @@ if (Meteor.isClient) {
     console.log("PennApps");
 
     // handle carousel
+    Meteor.startup(function(){
+        $('.carousel').enableTouch();
+        $('.carousel').on('swipeLeft', function(){
+            $('.carousel').carousel('next');
+        });
+        $('.carousel').on('swipeRight', function(){
+            $('.carousel').carousel('prev');
+        });
+    });
+
     Template.body.events({
         'click #carousel-left': function(){
             $('.carousel').carousel('prev');
         },
         'click #carousel-right': function(){
             $('.carousel').carousel('next');
-        }
+        },
     });
 }
