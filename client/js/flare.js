f9bbb42b449efc2fa0c44fdeec0003ac793dd3b1
hathix <neelmehta@college.harvard.edu>
2015-01-17 10:41:22 -0500

    Added routing

--- client/js/flare.js
+++ client/js/flare.js
@@ -1,6 +1,30 @@
 if (Meteor.isClient) {
     console.log("PennApps");
 
+    Router.configure({
+        layoutTemplate: 'layout'
+    });
+
+    Router.route('/', function () {
+        this.render('page_channels');
+    });
+
+    Router.route('/:channelname', function () {
+        var name = this.params.channelname;
+        this.redirect('/' + name + '/map');
+    });
+
+    Router.route('/:channelname/map', function () {
+        var name = this.params.channelname;
+        this.render('page_map');
+    });
+
+    Router.route('/:channelname/settings', function () {
+        var name = this.params.channelname;
+        this.render('page_channel_settings');
+    });
+
+    /*
     // handle carousel
     Session.set('title', 'Flare');
     Meteor.startup(function(){
@@ -25,6 +49,9 @@ if (Meteor.isClient) {
         'click #carousel-right': function(){
             $('.carousel').carousel('next');
         },
+        'click a[data-page]': function(e){
+            alert($(e.target).data('page'));
+        }
     });
 
     Template.body.helpers({
@@ -32,4 +59,5 @@ if (Meteor.isClient) {
             return Session.get('title');
         }
     });
+    */
 }
