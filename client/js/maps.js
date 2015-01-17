9de49a766af348643d3fe4a946ed9d132ba848ff
Max Wofford <max.speed.wofford@gmail.com>
2015-01-17 08:20:05 -0800

    App checks for user location before generating map

--- client/js/maps.js
+++ client/js/maps.js
@@ -1,14 +1,14 @@
 if (Meteor.isClient) {
   Meteor.startup(function() {
     GoogleMaps.load();
+    Geolocation.load();
   });
-  // Map view center hardcoded for now
-  Template.body.helpers({
-    mapOptions: function() {
+  Template.page_map.helpers({
+    exampleMapOptions: function() {
       // Make sure the maps API has loaded
       if (GoogleMaps.loaded()) {
         // We can use the `ready` callback to interact with the map API once the map is ready.
-        GoogleMaps.ready('myMap', function(map) {
+        GoogleMaps.ready('exampleMap', function(map) {
           // Add a marker to the map once it's ready
           var marker = new google.maps.Marker({
             position: map.options.center,
@@ -18,20 +18,10 @@ if (Meteor.isClient) {
 
         // Map initialization options
         return {
-          center: new google.maps.LatLng(-37.8136, 144.9631),
+          center: new google.maps.LatLng(Geolocation.latLng()[0] || 0, Geolocation.latLng()[1] || 0),
           zoom: 8
         };
       }
     }
   });
 }
-// To use this module, add the following div 
-// {{> googleMap name="myMap" options=exampleMapOptions}}
-
-// Examples:
-// Map marker in center of page
-
-// var marker = new google.maps.Marker({
-//   position: map.options.center,
-//   map: map.instance
-// });
