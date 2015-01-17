c853863855c969d35dd80d9f366cfd27d8117923
Max Wofford <max.speed.wofford@gmail.com>
2015-01-17 10:14:56 -0800

    Removed extra line in map.js

--- client/js/maps.js
+++ client/js/maps.js
@@ -3,28 +3,27 @@ if (Meteor.isClient) {
     GoogleMaps.load();
   });
   // Map view center hardcoded for now
-  var mapOptions = {center: new google.maps.LatLng(-37.8136, 144.9631), zoom: 8};
   Template.body.helpers({
     mapOptions: function() {
-    // Make sure the maps API has loaded
-    if (GoogleMaps.loaded()) {
-      // We can use the `ready` callback to interact with the map API once the map is ready.
-      GoogleMaps.ready('myMap', function(map) {
-        // Add a marker to the map once it's ready
-        var marker = new google.maps.Marker({
-          position: map.options.center,
-          map: map.instance
+      // Make sure the maps API has loaded
+      if (GoogleMaps.loaded()) {
+        // We can use the `ready` callback to interact with the map API once the map is ready.
+        GoogleMaps.ready('myMap', function(map) {
+          // Add a marker to the map once it's ready
+          var marker = new google.maps.Marker({
+            position: map.options.center,
+            map: map.instance
+          });
         });
-      });
 
-      // Map initialization options
-      return {
-        center: new google.maps.LatLng(-37.8136, 144.9631),
-        zoom: 8
-      };
+        // Map initialization options
+        return {
+          center: new google.maps.LatLng(-37.8136, 144.9631),
+          zoom: 8
+        };
+      }
     }
-  }
-});
+  });
 }
 // To use this module, add the following div 
 // {{> googleMap name="myMap" options=exampleMapOptions}}
