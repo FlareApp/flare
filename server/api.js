f31e3f57f1339e84fd99e50e5353d5c42fd7af1b
hathix <neelmehta@college.harvard.edu>
2015-01-18 05:26:29 -0500

    Added api sending from Pebble

--- server/api.js
+++ server/api.js
@@ -9,6 +9,25 @@ Meteor.startup(function(){
 
                 return JSON.stringify(channels.fetch());
             }
+        },
+        "/api/send": {
+            get: function(data){
+                var channelName = this.query.channel;
+
+                var id = Flares.insert({
+                    creator: null,
+                    location: { lat: 39.9516334, lon: -75.1911021 },
+                    created: new Date(),
+                    duration: 24 * 60 * 60,
+                    text: "Sent from my Pebble",
+                    media: []
+                });
+
+                // Tie it to the channel
+                Channels.update({ name: channelName }, {
+                    $push: { flares: id }
+                });
+            }
         }
     });
 });
